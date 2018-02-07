7eca14e32c4550e6a33dee51c41a416d6b8f5d55
Dylan Vaughn <dylancvaughn@gmail.com>
2018-02-06 20:52:49 -0800

    adding CrossAccountAccessId

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
+variable "cross_account_access_id" {
+  default = ""
+}
+
 resource "aws_cloudformation_stack" "upload_bucket" {
   name = "upload-bucket-stack"
   capabilities = ["CAPABILITY_IAM"]
   on_failure = "DELETE"
 
+  parameters {
+    CrossAccountAccessId = "${var.cross_account_access_id}"
+  }
   template_body = "${file("${path.module}/template.yaml")}"
 }
